11a6e64e7693cddd0bd42ded87ed34c9b28fb3d0
Vincenzo Passariello <passa97@gmail.com>
2017-08-25 14:09:35 +0200

    Add files via upload

---
+++ usernameAndPass.py
@@ -0,0 +1,8 @@
+print("Welcome. Please log in")
+usr = ""
+psw = ""
+while usr != "John Smith":
+    usr = input("Username: ")
+while psw != "cleverpass":
+    psw = input("Password: ")
+print("Login completed!")
