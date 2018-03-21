69ea490535960998e219ba7fc33ae98faae4434b
Vincenzo Passariello <passa97@gmail.com>
2018-03-21 22:09:34 +0100

    Update Student.py

--- Student.py
+++ Student.py
@@ -1,27 +1,31 @@
 class Student():
-	def __init__(self, name, surname, institute, mark):
-		self.n = name
-		self.s = surname
-		self.i = institute
-		self.m = mark
+    def __init__(self, name, surname, institute, mark):
+        self.n = name
+        self.s = surname
+        self.i = institute
+        self.m = mark
 		
 def printData(a):
-        x = print("Name is", a.n)
-        y = print("Surname is", a.s)
-        z = print("Institute is", a.i)
-        return(x, y, z)
+    x = print("Name is", a.n)
+    y = print("Surname is", a.s)
+    z = print("Institute is", a.i)
+    return(x, y, z)
 
 
 def multipleMarks(list1):
-	for i in range(10):
-		i = int(input("Mark: "))
-		list1.append(i)
-	return(list1)
+    flag = True
+    while flag:
+        try:
+            n = float(input("Mark (press any non-numeric key to exit the loop): "))
+            list1.append(n)
+        except(SyntaxError, TypeError, ValueError) as e:
+            flag = False
+    return(list1)
 
 def avg(a):
-        obj = a.m
-        mean = sum(obj)
-        print("The average of marks is", (mean/10))
+    obj = a.m
+    mean = sum(obj)
+    print("The average of marks is", (mean/10))
 
 list1 = []
 print("This program outputs one student's data and the average of his/her marks.\n")
